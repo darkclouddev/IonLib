b8d428e5841acc67f7cf24172d46005ab26a04c2
Andy Ion <andy@andyion.ru>
2016-11-22 17:12:51 +0300

    RFC2898 (PBKDF2) implementation

---
+++ IonLib/cryptoservices/PBKDF2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace IonLib.cryptoservices
+{
+	public static class PBKDF2
+	{
+		const int Iterations = 1000;
+
+		public static byte[] Encrypt(string password, byte[] salt)
+		{
+			Rfc2898DeriveBytes pbkdf2 = new Rfc2898DeriveBytes(password, salt);
+			return pbkdf2.GetBytes(16);
+		}
+
+		public static byte[] GenerateSalt()
+		{
+			byte[] salt = new byte[8];
+
+			RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+			rng.GetBytes(salt);
+
+			return salt;
+		}
+	}
+}
